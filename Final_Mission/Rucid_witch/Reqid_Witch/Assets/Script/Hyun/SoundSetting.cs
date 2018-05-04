f862fb2efea67ccf069b5e4b4e9ac179809144bf
wooloves39 <wooloves39@users.noreply.github.com>
2018-05-04 23:37:02 +0900

    사운드 셋팅

---
+++ Final_Mission/Rucid_witch/Reqid_Witch/Assets/Script/Hyun/SoundSetting.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundSetting : MonoBehaviour {
+
+	private AudioSource CompleteSound;
+	public float volumeBalance=1.0f;
+	private void Awake()
+	{
+		CompleteSound = GetComponent<AudioSource>();
+		CompleteSound.volume = Singletone.Instance.Sound*volumeBalance;
+	}
+}

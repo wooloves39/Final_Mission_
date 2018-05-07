2901821dd311f6b64debc36a412f190c09505022
FlyBKang <cdcd8894@naver.com>
2018-05-07 09:11:30 +0900

    skillsoundset + 파티클 수정

---
+++ Final_Mission/Rucid_witch/Reqid_Witch/Assets/Script/Skill/Skill_Sound_Set.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skill_Sound_Set : MonoBehaviour {
+
+	private AudioSource sound;
+	public float Wait_Time;
+	public AudioClip play;
+
+	// Use this for initialization
+	void Start () {
+		sound = GetComponent<AudioSource> ();
+		sound.volume = Singletone.Instance.Sound;
+	}
+	void OnEnable()
+	{
+		sound = GetComponent<AudioSource> ();
+		sound.volume = Singletone.Instance.Sound;
+		Invoke ("PlaySound", Wait_Time);
+	}
+	void PlaySound()
+	{
+		sound.clip = play;
+		sound.Play ();
+	}
+}

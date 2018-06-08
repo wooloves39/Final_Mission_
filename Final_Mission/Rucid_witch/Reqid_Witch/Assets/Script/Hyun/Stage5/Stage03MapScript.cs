5333a42c579829de1b8594bbb33828cb92ec2de4
wooloves39 <wooloves39@users.noreply.github.com>
2018-06-08 14:55:09 +0900

    비제맵 셋팅

---
+++ Final_Mission/Rucid_witch/Reqid_Witch/Assets/Script/Hyun/Stage5/Stage03MapScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stage03MapScript : MonoBehaviour {
+	public GameObject[] particleOn;
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(ParticleOnCor());
+	}
+	IEnumerator ParticleOnCor()
+	{
+		particleOn[0].SetActive(true);
+		yield return new WaitForSeconds(1.5f);
+		particleOn[1].SetActive(true);
+		yield return new WaitForSeconds(1.5f);
+		particleOn[2].SetActive(true);
+		yield return new WaitForSeconds(1.5f);
+		particleOn[3].SetActive(true);
+		yield return new WaitForSeconds(1.5f);
+		particleOn[4].SetActive(true);
+	}
+}

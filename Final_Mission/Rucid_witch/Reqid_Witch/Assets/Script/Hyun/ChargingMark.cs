16d9a103aca903a0b77dcffb823f4f1bede9b11e
wooloves39 <wooloves39@users.noreply.github.com>
2018-07-01 03:26:12 +0900

    stage6 UI 2차 작업완료(쿨타임 데이터만있으면 적용ㅁ

--- Final_Mission/Rucid_witch/Reqid_Witch/Assets/Script/Hyun/ChargingMark.cs
+++ Final_Mission/Rucid_witch/Reqid_Witch/Assets/Script/Hyun/ChargingMark.cs
@@ -7,13 +7,37 @@ public class ChargingMark : MonoBehaviour {
 	public bool isCool = false;
 	private Slider slider;
 	public float MPGage;
+	private PlayerState playerState;
+	private SpriteRenderer spriteRenderer;
+	//쿨타임 어딧는지 모름.
 	// Use this for initialization
-	void Start () {
+	private void Awake()
+	{
 		slider = GetComponentInChildren<Slider>();
+		playerState = GetComponentInParent<PlayerState>();
+		spriteRenderer = GetComponent<SpriteRenderer>();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+	private void OnEnable()
+	{
+		if (playerState.Mp < MPGage) { StartCoroutine(MpLack()); }
+	}
+	private void OnDisable()
+	{
+		Color color = spriteRenderer.color;
+		color.a = 1;
+		spriteRenderer.color= color;
+	}
+	IEnumerator MpLack()
+	{
+		Color color = spriteRenderer.color;
+		float Alpha = -10.0f;
+		while(playerState.Mp < MPGage)
+		{
+			color.a += Alpha/255.0f;
+			spriteRenderer.color = color;
+			if (color.a <= 0.0f|| color.a >= 1.0f)
+				Alpha *= -1;
+			yield return null;
+		}
 	}
 }
---
+++ Final_Mission/Rucid_witch/Reqid_Witch/Assets/Script/Hyun/MapScriptSt6.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapScriptSt6 : MonoBehaviour {
+	public Transform[] RotateObj;
+	public float RotateAngle=10.0f;
+	// Update is called once per frame
+	void Update () {
+		for (int i = 0; i < RotateObj.Length; ++i)
+		{
+			if (i % 2 == 0)
+				RotateObj[i].transform.Rotate(Vector3.up,  RotateAngle);
+			else
+			{
+				RotateObj[i].transform.Rotate(Vector3.down,  RotateAngle);
+			}
+		}
+	}
+}

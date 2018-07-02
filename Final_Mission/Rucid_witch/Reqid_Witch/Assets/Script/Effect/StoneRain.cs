e20d94401786cf480f6382d80f048fba8a658fac
FlyBKang <cdcd8894@naver.com>
2018-07-02 14:13:17 +0900

    땅갈라지는 효과 마무리

---
+++ Final_Mission/Rucid_witch/Reqid_Witch/Assets/Script/Effect/StoneRain.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoneRain : MonoBehaviour {
+    private float time;
+    public float height;
+    public float UpSpeed;
+    public float DownSpeed;
+    void OnEnable()
+    {
+        this.transform.position = Vector3.zero;
+        time = Time.deltaTime;
+        StartCoroutine("stone");
+    }
+    IEnumerator stone()
+    {
+        while (true)
+        {
+            if (this.transform.position.y < height)
+                this.transform.position += Vector3.up * UpSpeed;
+            else
+                break;
+            yield return new WaitForSeconds(time);
+        }
+        while (true)
+        {
+            if (this.transform.position.y > 0)
+                this.transform.position -= Vector3.up * DownSpeed;
+            else
+                break;
+            yield return new WaitForSeconds(time);
+        }
+    }
+}

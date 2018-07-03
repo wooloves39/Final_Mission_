425650e944f5d097e98269266d73e3d371c21218
wooloves39 <wooloves39@users.noreply.github.com>
2018-07-03 23:31:07 +0900

    칼리 파티클2차, Ready씬 UI적용

---
+++ Final_Mission/Rucid_witch/Reqid_Witch/Assets/Script/Hyun/Lightsaber.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lightsaber : MonoBehaviour {
+    LineRenderer lindRend;
+    public Transform startPos;
+    public Transform endPos;
+
+    private float textureOffset = 0f;
+	// Use this for initialization
+	void Start () {
+        lindRend = GetComponent<LineRenderer>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        lindRend.SetPosition(0, startPos.position);
+        lindRend.SetPosition(1, endPos.position);
+
+        textureOffset -= Time.deltaTime*2f;
+        if (textureOffset < -10f)
+        {
+            textureOffset += 10f;
+        }
+        lindRend.sharedMaterials[0].SetTextureOffset("_MainTex", new Vector2(textureOffset, 0f));
+    }
+}
---
+++ Final_Mission/Rucid_witch/Reqid_Witch/Assets/Script/Hyun/ReadySence/OperationHelper.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OperationHelper : MonoBehaviour {
+	private SpriteRenderer[] Operations;
+	private int count = 0;
+	private Color operationColor=Color.white;
+	private bool change = false;
+	private bool Alpha = false;
+	public float val = 1.0f;
+	// Use this for initialization
+	void Start () {
+		operationColor.a = 0;
+		count = 0;
+		Operations = GetComponentsInChildren<SpriteRenderer>();
+		for(int i=0;i< Operations.Length; ++i)
+		{
+			Operations[i].color = operationColor;
+			Operations[i].gameObject.SetActive(false);
+		}
+		Operations[count].gameObject.SetActive(true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		Operations[count].color = operationColor;
+		operationColor.a += val / 255.0f;
+		if (operationColor.a >= 1.0f)
+		{
+			Alpha = true;
+			val *= -1;
+		}
+		if(operationColor.a <= 0.0f&&Alpha)
+		{
+			Operations[count].gameObject.SetActive(false);
+			count++;
+			if (count >= Operations.Length) count = 0;
+			Alpha = false;
+			val *= -1;
+			Operations[count].gameObject.SetActive(true);
+		}
+		
+	}
+	
+	void trueOn(int count)
+	{
+		Operations[count].gameObject.SetActive(true);
+	}
+}

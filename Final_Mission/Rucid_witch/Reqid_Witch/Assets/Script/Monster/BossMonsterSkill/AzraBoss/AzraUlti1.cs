b106805f82409f159e7c6bb489ae5244f39d6c1b
FlyBKang <cdcd8894@naver.com>
2018-08-02 14:14:14 +0900

    아즈라 궁극기 중간저장 1

---
+++ Final_Mission/Rucid_witch/Reqid_Witch/Assets/Script/Monster/BossMonsterSkill/AzraBoss/AzraUlti1.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AzraUlti1 : MonoBehaviour {
+    private TreeSkill[] tree;
+	// Use this for initialization
+	void Start () {
+        tree = GetComponentsInChildren<TreeSkill>();
+	}
+    void OnEnable()
+    {
+        StartCoroutine("Skill");
+    }
+    IEnumerator Skill()
+    {
+        
+    }
+}
--- Final_Mission/Rucid_witch/Reqid_Witch/Assets/TreeSkill.cs
+++ Final_Mission/Rucid_witch/Reqid_Witch/Assets/TreeSkill.cs
@@ -6,6 +6,7 @@ public class TreeSkill : MonoBehaviour {
 
     public float offset = -7.0f;
 	public float glowTime = 1.0f;
+    public float upsize = -2.0f;
 	public Transform target;
 	private Vector3 SetPos;
 	// Use this for initialization
@@ -13,16 +14,12 @@ public class TreeSkill : MonoBehaviour {
 	{
 		StartCoroutine("GrowUp");
     }
-    void OnDisable()
-	{
-		StopCoroutine("GrowUp");
-	}
     IEnumerator GrowUp()
     {
 		this.transform.position += this.transform.up * offset;
         float cycle = 0.05f;
         float size = 7.0f / (glowTime / cycle);
-        while (this.transform.position.y < -2.0f)
+        while (this.transform.position.y < upsize)
         {
             this.transform.Translate( target.up * size);
             yield return new WaitForSeconds(cycle);

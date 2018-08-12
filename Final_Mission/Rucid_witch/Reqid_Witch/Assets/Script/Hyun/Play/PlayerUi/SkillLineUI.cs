fd32cf09f3f659c5e000d7a60f27544b86fa6bc6
wooloves39 <wooloves39@users.noreply.github.com>
2018-08-13 00:53:27 +0900

    stage 전체점검

--- Final_Mission/Rucid_witch/Reqid_Witch/Assets/Script/Hyun/Play/PlayerUi/SkillLineUI.cs
+++ Final_Mission/Rucid_witch/Reqid_Witch/Assets/Script/Hyun/Play/PlayerUi/SkillLineUI.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class SkillLineUI : MonoBehaviour {
 	public GameObject[] SkillSet;
+	private void Awake()
+	{
+		for (int i = 0; i < SkillSet.Length; ++i)
+		{
+			SkillSet[i].SetActive(false);
+		}
+	}
 	private void OnEnable()
 	{
 		SkillSet[LineDraw.curType].SetActive(true);

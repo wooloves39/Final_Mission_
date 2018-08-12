e995ece69860125d7e5b3e32000f2685f2655239
hyunwooJeon <wooloves39@gmail.com>
2018-08-13 00:09:56 +0900

    메타 복구

--- Final_Mission/Rucid_witch/Reqid_Witch/Assets/Script/Singletone.cs
+++ Final_Mission/Rucid_witch/Reqid_Witch/Assets/Script/Singletone.cs
@@ -27,7 +27,7 @@ public class Singletone
 	public string name= "Reqid_Witch";
 	public int stage = 6;
 	public string saveTime;
-	public int[] Myskill = { 1, 2, 4 };
+	public int[] Myskill = { 1, 0, 4 };
 
 	
 	public void Save(string FileName)

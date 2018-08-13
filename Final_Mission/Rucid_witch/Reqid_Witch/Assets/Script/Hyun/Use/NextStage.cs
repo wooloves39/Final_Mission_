396741d4c114d5ec21b238023968664003951c3e
wooloves39 <wooloves39@users.noreply.github.com>
2018-08-13 20:54:53 +0900

    Ending준비

--- Final_Mission/Rucid_witch/Reqid_Witch/Assets/Script/Hyun/Use/NextStage.cs
+++ Final_Mission/Rucid_witch/Reqid_Witch/Assets/Script/Hyun/Use/NextStage.cs
@@ -5,7 +5,7 @@ public class NextStage : MonoBehaviour
 {
 	private SceneChange sceneChange;
 	private bool NextOn = false;
-
+	public bool isLast = false;
 	public void setNextOn(bool val) { NextOn = val; }
 	public bool getNextOn() { return NextOn; }
 	private void Awake()
@@ -16,16 +16,23 @@ public class NextStage : MonoBehaviour
 	{
 		if (other.gameObject.CompareTag("Player") && NextOn)
 		{
-			if (Singletone.Instance.stage == -1)
-				Singletone.Instance.stage = 1;
+			if (!isLast)
+			{
+				if (Singletone.Instance.stage == -1)
+					Singletone.Instance.stage = 1;
+				else
+				{
+					++Singletone.Instance.stage;
+				}//스테이지 정보, 속성 추가등 부가 요소 정리
+
+				//ReadyScene으로!
+				Debug.Log(Singletone.Instance.stage + " 다음씬");
+				sceneChange.sceneChange("Ready");
+			}
 			else
 			{
-				++Singletone.Instance.stage;
-			}//스테이지 정보, 속성 추가등 부가 요소 정리
-
-			//ReadyScene으로!
-			Debug.Log(Singletone.Instance.stage + " 다음씬");
-			sceneChange.sceneChange("Ready");
+				sceneChange.sceneChange("GameEnd");
+			}
 		}
 	}
 
